a09ddb944248262d50a8ff63d925795aa556adab
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-03-24 20:59:12 -0400

    ok

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+           // clearing the page after the searh button been push
             $(".City").text("");
             $(".description").text("");
             $('#icon').empty();
@@ -82,41 +83,35 @@ $(document).ready(function () {
             url: queryURL2,
             method: "GET"
         }).then(function (response2) {
-            console.log(response2)
+            // clearing up the card div
             $(".card").empty();
-
-            console.log(response2.list[1]);
-            var tempd = (response2.list[1].main.temp - 273.15) * 1.80 + 32;
-            console.log(tempd);
-
+            // loop fof 1200 pm for the next five days
            for (i = 0; i < response2.list.length; i++) {
                if (response2.list[i].dt_txt.indexOf("12:00:00") !== -1) {
-                console.log((response2.list[i].main.temp - 273.15) * 1.80 + 32); 
-             
-           
+             // made a card div   
             var card = $("<div>").addClass("card");
+            // made a cardbody div 
             var cardBody = $("<div>").addClass("card-body");
+            // made a card date 
             var cardDate = $("<h5>").addClass("card-body").text(new Date(response2.list[i].dt_txt).toLocaleDateString());
+            // made a card Image tag
             var cardImg = $("<img>").addClass("card-img").attr("src","http://openweathermap.org/img/w/"+response2.list[i].weather[0].icon + ".png");
-            
+            // made a temp paragraph tag
             var cardtemp = $("<p>").addClass("card-text").text("Temperature: " +response2.list[i].main.temp_max)
-
+            // made a Humidity paragraph tag
             var cardHum = $("<p>").addClass("card-text").text("Humidity:" +response2.list[i].main.humidity);
+            // append everything inside the card body
             cardBody.append(cardDate,cardImg,cardtemp,cardHum);
+            // then append the card body in the card div
             card.append(cardBody);
+            //then append the card in the forecast div we made in the HTML
             $("#forecast").append(card);
 
                }
                
            }
-           console.log(response2.list[8])
-           // display the next 5 days
-
-           // put tye following 5 day on the card
-
-           // display the 5 days temperature forecast
-
-           //$(".wind").append("wind Speed: ", + response2.listwind.speed);  
+          
+         
             
 
 
